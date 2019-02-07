270198aecf0767d2f94c57b09bd0a0d3aeb77ff5
Khaleefa <ktkhaleefakt@gmail.com>
2019-02-07 14:58:23 +0530

    ex10 to ex12

---
+++ ex10.py
@@ -0,0 +1,14 @@
+tabby_cat="\t I'am tabbed in."
+persion_cat="I'am split \n on a line"
+backslash_cat="I am \\a \\ cat"
+
+fat_cat="""
+I'll do a list:
+\t* Cat food
+\t* Fishes
+\t* Cat nip\n\t* Grass
+"""
+print(tabby_cat)
+print(persion_cat)
+print(backslash_cat)
+print(fat_cat)
---
+++ ex12.py
@@ -0,0 +1,4 @@
+age=input("enter your age")
+height=input("how tall are you")
+weight=input ("how much do you weight")
+print(f"so you are {age} years old, {height}tall and {weight}lbs heavy")
