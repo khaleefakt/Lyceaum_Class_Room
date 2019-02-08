d25c6d3af1b7684ed1eaf2cb979b2791090efcf0
Khaleefa <ktkhaleefakt@gmail.com>
2019-02-08 11:33:49 +0530

    find largest using direct datas given in an array

--- Programs/largest.py
+++ Programs/largest.py
@@ -1,8 +1,9 @@
-   
-a=[]
-n=int(input("Enter number of elements:"))
-for i in range(1,n+1):
-    b=int(input("Enter element:"))
-    a.append(b)
-a.sort()
-print('Largest element is:',a[n-1])
+data=[4,1,2,3,5]
+largest=0
+#n=int(input("Enter number of elements:"))
+for i in data:
+#    b=int(input("Enter element:"))
+    if i > largest:
+        max = i
+#    a.append(b)
+print('Largest element is:',i)
